02f5c8e9f20dcfab456fe454be27a47fa828fa94
Juan <jmoreno@novopayment.com>
2023-07-03 17:01:26 -0500

    202307031701 bonjour

---
+++ Bonjour.swift
@@ -0,0 +1,16 @@
+//
+//  Bonjour.swift
+//  IntegracionFreeToGameAlamofire
+//
+//  Created by Juan Manuel Moreno on 3/07/23.
+//
+
+import Foundation
+
+public class Bonjour {
+    public init() {
+    }
+    public func salut() {
+        print("Bonjour")
+    }
+}
